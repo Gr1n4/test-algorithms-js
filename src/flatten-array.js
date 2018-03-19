e1dde1ade8108168fd537fc08b2db21d3b242f3c
Tony <AVStarikovich@ya.ru>
2018-03-19 22:27:40 +0300

    update flatten array solution

--- src/flatten-array.js
+++ src/flatten-array.js
@@ -4,7 +4,20 @@ const FlattenArray = (function() {
   function FlattenArray() {}
 
   FlattenArray.prototype.flatten = function(arr) {
+    if (!(arr instanceof Array)) {
+      throw new Error('Is not array');
+    }
 
+    for (let i = 0; i < arr.length; i++) {
+      if (!(arr[i] instanceof Array)) {
+        continue;
+      }
+
+      arr.splice(i, 1, ...arr[i]);
+      i--;
+    }
+
+    return arr;
   };
 
   return FlattenArray;
--- src/flatten-array.spec.js
+++ src/flatten-array.spec.js
@@ -1,18 +1,25 @@
 'use strict';
 
 describe('flatten array', () => {
-  let arr
-    , fa;
+  let fa;
 
   beforeEach(() => {
     fa = new FlattenArray();
   });
 
+  it('should create instance', () => {
+    expect(fa instanceof FlattenArray).toBeTruthy();
+  });
+
+  it('should defined method', () => {
+    expect(fa.flatten).toBeDefined();
+  });
+
   it('should exception is not array', () => {
-    expect(fa.flatten('abs')).toThrowError('Is not array');
-    expect(fa.flatten({a: 'b'})).toThrowError('Is not array');
-    expect(fa.flatten(2)).toThrowError('Is not array');
-    expect(fa.flatten(true)).toThrowError('Is not array');
+    expect(() => fa.flatten('abs')).toThrowError('Is not array');
+    expect(() => fa.flatten({a: 'b'})).toThrowError('Is not array');
+    expect(() => fa.flatten(2)).toThrowError('Is not array');
+    expect(() => fa.flatten(true)).toThrowError('Is not array');
   });
 
   it('should return correct flatten array', () => {
